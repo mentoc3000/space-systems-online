898e70821faf62a557ebcd8a17a7dd97997b6f28
JP Sheehan <jp.sheehan2@gmail.com>
2017-01-15 17:59:10 -0500

    removed transpiled js files

--- public/app/main.js
+++
@@ -1,5 +0,0 @@
-/// <reference path="../../node_modules/@types/node/index.d.ts" />
-"use strict";
-var platform_browser_dynamic_1 = require('@angular/platform-browser-dynamic');
-var app_module_1 = require('./app.module');
-platform_browser_dynamic_1.platformBrowserDynamic().bootstrapModule(app_module_1.AppModule);

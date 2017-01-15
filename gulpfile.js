09f8015dba9a9855725174e30c86d4ea31a4db47
JP Sheehan <jp.sheehan2@gmail.com>
2017-01-15 18:25:35 -0500

    migrated css to scss, added gulp processes

--- gulpfile.js
+++ gulpfile.js
@@ -11,41 +11,47 @@ var concat = require('gulp-concat');
 var rename = require('gulp-rename');
 var uglify = require('gulp-uglify');
 var ts = require('gulp-typescript');
-
+var changed = require('gulp-changed');
+var changedInPlace = require('gulp-changed-in-place');
+var logger = require('gulp-print');
 
 // Main Task
 
 gulp.task('default', function() {
-   gulp.watch([
-      'lib/**/*.js',
-      '!lib/script/**',
-      'test/**/*Test.js'
-   ],[
-      'mocha'
-   ]);
+  gulp.watch([
+    'lib/**/*.js',
+    '!lib/script/**',
+    'test/**/*Test.js'
+  ], [
+    'mocha'
+  ]);
 
-   gulp.watch([
-      'app/**/*.js',
-      'spec/**/*Spec.js'
-   ],[
-      'karma'
-   ]);
+  gulp.watch([
+    'app/**/*.js',
+    'spec/**/*Spec.js'
+  ], [
+    'karma'
+  ]);
 });
 
 
 // Testing
 
 gulp.task('mocha', function() {
-   return gulp.src(['test/**/*Test.js'], { read: false })
-      .pipe(mocha({ reporter: 'list' }))
-      .on('error', gutil.log);
+  return gulp.src(['test/**/*Test.js'], {
+      read: false
+    })
+    .pipe(mocha({
+      reporter: 'list'
+    }))
+    .on('error', gutil.log);
 });
 
 gulp.task('karma', function(done) {
-   new KarmaServer({
-      configFile: __dirname + '/karma.conf.js',
-      singleRun: true
-   }, done).start();
+  new KarmaServer({
+    configFile: __dirname + '/karma.conf.js',
+    singleRun: true
+  }, done).start();
 });
 
 
@@ -70,20 +76,22 @@ gulp.task('karma', function(done) {
 
 // Building
 
-// var jsDest = 'app/app-content/js';
-// var cssDest = 'app/app-content/css';
-//
-// gulp.task('sass', function() {
-//    return gulp.src('scss/**/*.scss')
-//       .pipe(sass().on('error', sass.logError))
-//       .pipe(gulp.dest('app/app-content/css'))
-//       .pipe(concat('style.css'))
-//       .pipe(gulp.dest(cssDest));
-// });
+
+gulp.task('sass', function() {
+  return gulp.src('public/**/*.scss')
+    // .pipe(changedInPlace('.'))
+    .pipe(logger())
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('./public/'))
+    /* Gulp takes everything that's a wildcard
+     * or a globstar into its virtual file name. */
+});
 
 var tsProject = ts.createProject('tsconfig.json');
-gulp.task('tsc',function() {
+gulp.task('tsc', function() {
   return tsProject.src()
+    .pipe(changedInPlace('.'))
+    .pipe(logger())
     .pipe(tsProject())
     .pipe(gulp.dest('.'));
 });
@@ -91,13 +99,14 @@ gulp.task('tsc',function() {
 
 // Serving
 
-gulp.task('serve', ['sass'], function() {
-   return nodemon({
-      script: 'server.js',
-      ext: 'html js css',
-      ignore: [
-         'lib/gmat/*',
-         'lib/gmat-dist/*'
-      ],
-      tasks: ['sass'] });
+gulp.task('serve', ['sass', 'tsc'], function() {
+  return nodemon({
+    script: 'server.js',
+    ext: 'html js css',
+    ignore: [
+      'lib/gmat/*',
+      'lib/gmat-dist/*'
+    ],
+    tasks: ['sass', 'tsc']
+  });
 });

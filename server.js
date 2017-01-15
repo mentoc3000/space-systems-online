33ec1fdb81bf397a5b352a24f23d8887ba9cd512
JP Sheehan <jp.sheehan2@gmail.com>
2017-01-15 17:16:08 -0500

    minor tweaks

--- server.js
+++ server.js
@@ -6,8 +6,10 @@ var express = require('express');
 var app = express();
 
 app.set('port', process.env.PORT || 3000);
-app.use('/bower_components', express.static(__dirname + '/bower_components/'));
-app.use('/node_modules', express.static(__dirname + '/node_modules/'));
+
+
+app.use('/bower_components', express.static(path.join(__dirname,'bower_components')));
+app.use('/node_modules', express.static(path.join(__dirname,'node_modules')));
 app.use(express.static(path.join(__dirname, 'public')));
 
 

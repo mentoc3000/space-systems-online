99fe1acb8714b22c8732b301eada745face34db4
JP Sheehan <jp.sheehan2@gmail.com>
2017-01-15 16:01:45 -0500

    Basic express server

---
+++ public/app/simulator/simulator.module.js
@@ -0,0 +1,32 @@
+"use strict";
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+var __metadata = (this && this.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
+var core_1 = require('@angular/core');
+var simulator_routing_module_1 = require('./simulator-routing.module');
+var simulator_component_1 = require('./simulator.component');
+var SimulatorModule = (function () {
+    function SimulatorModule() {
+    }
+    SimulatorModule = __decorate([
+        core_1.NgModule({
+            imports: [
+                simulator_routing_module_1.SimulatorRoutingModule
+            ],
+            declarations: [
+                simulator_component_1.SimulatorComponent
+            ],
+            providers: []
+        }), 
+        __metadata('design:paramtypes', [])
+    ], SimulatorModule);
+    return SimulatorModule;
+}());
+exports.SimulatorModule = SimulatorModule;
+//# sourceMappingURL=simulator.module.js.map
---
+++ server.js
@@ -0,0 +1,49 @@
+'use strict';
+
+var path = require('path');
+var express = require('express');
+
+var app = express();
+
+app.set('port', process.env.PORT || 3000);
+app.use('/bower_components', express.static(__dirname + '/bower_components/'));
+app.use('/node_modules', express.static(__dirname + '/node_modules/'));
+app.use(express.static(path.join(__dirname, 'public')));
+
+/*
+app.get('/', function (req, res) {
+  res.send('Hello World!')
+})
+*/
+
+// Serve the website
+
+var server = app.listen(app.get('port'),function(){
+   // var host = server.address().address;
+   var host = '::';
+   var port = server.address().port;
+
+   if (host === '::') {
+      host = 'localhost';
+   }
+
+   console.log('Server listening at http://' + host + ':' + port);
+});
+
+
+// Close server correctly
+
+function exitHandler(options, err) {
+   server.close();
+   if (err) {
+      console.log(err.stack);
+   }
+   console.log('Server closed.');
+   if (options.exit) {
+      process.exit();
+   }
+}
+
+// process.on('exit', exitHandler.bind(null, {exit: true}));
+process.on('SIGINT', exitHandler.bind(null, {exit: true}));
+process.on('uncaughtException', exitHandler.bind(null, {exit: true}));

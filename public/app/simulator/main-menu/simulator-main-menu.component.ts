18701e1cb9b914343af118bc98810cc6c1b01328
JP Sheehan <jp.sheehan2@gmail.com>
2017-01-20 14:14:31 -0500

    simulator main menu styling complete

--- public/app/simulator/main-menu/simulator-main-menu.component.ts
+++ public/app/simulator/main-menu/simulator-main-menu.component.ts
@@ -5,7 +5,6 @@ import { ActivatedRoute }       from '@angular/router';
   moduleId: module.id,
   selector: 'sso-sim-main-menu',
   templateUrl: 'simulator-main-menu.component.html',
-  // template: '<h3>main-menu</h3>'
-  // styleUrls: ['simulator-main-menu.component.css']
+  styleUrls: ['simulator-main-menu.component.css']
 })
 export class SimulatorMainMenuComponent { }

60f0393d407acc86dcff43ef7161569836461193
JP Sheehan <jp.sheehan2@gmail.com>
2017-01-19 19:20:20 -0500

    Created simulator earth component framework

---
+++ public/app/simulator/earth/simulator-earth.component.ts
@@ -0,0 +1,7 @@
+import { Component }        from '@angular/core';
+
+@Component({
+  moduleId: module.id,
+  template: '<h5>SimulatorEarthComponent</h5>'
+})
+export class SimulatorEarthComponent { }
---
+++ public/app/simulator/earth/simulator-earth.module.ts
@@ -0,0 +1,16 @@
+import { NgModule }         from '@angular/core';
+
+import { SimulatorEarthComponent }  from './simulator-earth.component';
+
+@NgModule({
+  imports: [
+
+  ],
+  declarations: [
+    SimulatorEarthComponent
+  ],
+  providers: [
+
+  ]
+})
+export class SimulatorEarthModule { }

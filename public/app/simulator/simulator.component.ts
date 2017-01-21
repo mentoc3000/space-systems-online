b8934025b09e7e4ebb1b44d0ef2c57f426bb0b53
JP Sheehan <jp.sheehan2@gmail.com>
2017-01-20 22:20:40 -0500

    moved simulator html into component

--- public/app/simulator/simulator.component.ts
+++ public/app/simulator/simulator.component.ts
@@ -2,7 +2,10 @@ import { Component }          from '@angular/core';
 
 @Component({
   moduleId: module.id,
-  templateUrl: 'simulator.component.html',
+  template: `<div class="simulator">
+    <sso-sim-main-menu></sso-sim-main-menu>
+    <router-outlet></router-outlet>
+  </div>`,
   styleUrls: ['simulator.component.css']
 })
 export class SimulatorComponent { }

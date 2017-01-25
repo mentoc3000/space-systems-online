4f3d3f4c2520162f06940615a62df7fefce51930
JP Sheehan <jp.sheehan2@gmail.com>
2017-01-24 21:13:55 -0500

    Named class tests added

--- client/app/simulator/shared/classes/named.class.test.ts
+++ client/app/simulator/shared/classes/named.class.test.ts
@@ -1,71 +1,35 @@
 import { expect } from 'chai';
+import { Named } from './named.class';
 
 describe('Named Class test', () => {
 
-   it('true is true', () => expect(true).to.equal(true));
-
-/*
-   beforeEach(angular.mock.module('SSOApp'));
-
-   var NamedClass, named, name;
-   beforeEach(inject(function(_NamedClass_){
-      NamedClass = _NamedClass_;
-      named = new NamedClass();
+   let named: Named;
+   let name: string;
+   beforeEach(() => {
+      named = new Named();
       name = 'big ass dish';
-   }));
+   });
 
-   it('has no name when first created',function() {
-      expect(named.getName()).toEqual('');
+   it('has no name when first created', function() {
+      expect(named.getName()).to.equal('');
    });
 
    it('sets a name', function() {
       expect(function() {
          named.setName(name);
-      }).not.toThrow();
+      }).not.to.throw();
    });
 
    it('gets the set name', function() {
       named.setName(name);
-      expect(named.getName()).toEqual(name);
+      expect(named.getName()).to.equal(name);
    });
-   
+
    it('deletes the name', function() {
       named.setName(name);
       named.deleteName();
-      expect(named.getName()).toEqual('');
+      expect(named.getName()).to.equal('');
    });
-
-   it('throws an error if the name is not a string',function() {
-      var err = 
-      expect(function() {
-         var badName = {
-            x: 123
-         };
-         named.setName(badName);
-      }).toThrowError(TypeError);
-
-      expect(function() {
-         var badName = 123;
-         named.setName(badName);
-      }).toThrowError(TypeError);
-      
-      expect(function() {
-         var badName;
-         named.setName(badName);
-      }).toThrowError(TypeError);
-      
-      expect(function() {
-         var badName = null;
-         named.setName(badName);
-      }).toThrowError(TypeError);
-      
-      expect(function() {
-         var badName = true;
-         named.setName(badName);
-      }).toThrowError(TypeError);
-      
-   });
-*/
 
 });
 

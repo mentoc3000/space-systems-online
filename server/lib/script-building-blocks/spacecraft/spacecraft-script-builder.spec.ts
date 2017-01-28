51789c4b8dd85526ec1f87bcf9462fe3abaf9d63
JP Sheehan <jp.sheehan2@gmail.com>
2017-01-28 12:14:53 -0500

    added spacecraft script builder

---
+++ server/lib/script-building-blocks/spacecraft/spacecraft-script-builder.spec.ts
@@ -0,0 +1,77 @@
+import {
+  Epoch,
+  CartesianOrbit,
+  KeplerianOrbit,
+  spacecraftScriptBuilder
+} from './spacecraft-script-builder';
+
+describe('spacecraft script builder', () => {
+
+  let epoch: Epoch;
+  let cartesianOrbit: CartesianOrbit;
+  let keplerianOrbit: KeplerianOrbit;
+  let name: string;
+  let output: string;
+
+  function rand(): number {
+    return 20 * (Math.random() - 0.5);
+  }
+
+  beforeEach(() => {
+    name = 'beautifulSpaceship1';
+    epoch = {
+      dateFormat: 'Julian',
+      epoch: '2017-01-13'
+    };
+    cartesianOrbit = {
+      system: 'cartesian',
+      x: rand(),
+      y: rand(),
+      z: rand(),
+      vx: rand(),
+      vy: rand(),
+      vz: rand()
+    };
+    keplerianOrbit = {
+      system: 'keplerian',
+      sma: rand(),
+      ecc: rand(),
+      inc: rand(),
+      raan: rand(),
+      aop: rand(),
+      ta: rand()
+    };
+  });
+
+  it('builds a default spacecraft', () => {
+    output = spacecraftScriptBuilder(name);
+    expect(output).toContain('Create Spacecraft ' + name);
+  });
+
+  it('builds a spacecraft with an epoch', () => {
+    output = spacecraftScriptBuilder(name, epoch);
+    expect(output).toContain(name + '.DateFormat = ' + epoch.dateFormat);
+    expect(output).toContain(name + '.Epoch = ' + epoch.epoch);
+  });
+
+  it('builds a spacecraft with a cartesian orbit', () => {
+    output = spacecraftScriptBuilder(name, undefined, cartesianOrbit);
+    expect(output).toContain(name + '.X = ' + cartesianOrbit.x);
+    expect(output).toContain(name + '.Y = ' + cartesianOrbit.y);
+    expect(output).toContain(name + '.Z = ' + cartesianOrbit.z);
+    expect(output).toContain(name + '.VX = ' + cartesianOrbit.vx);
+    expect(output).toContain(name + '.VY = ' + cartesianOrbit.vy);
+    expect(output).toContain(name + '.VZ = ' + cartesianOrbit.vz);
+  });
+
+  it('builds a spacecraft with a keplerian orbit', () => {
+    output = spacecraftScriptBuilder(name, undefined, keplerianOrbit);
+    expect(output).toContain(name + '.SMA = ' + keplerianOrbit.sma);
+    expect(output).toContain(name + '.ECC = ' + keplerianOrbit.ecc);
+    expect(output).toContain(name + '.INC = ' + keplerianOrbit.inc);
+    expect(output).toContain(name + '.RAAN = ' + keplerianOrbit.raan);
+    expect(output).toContain(name + '.AOP = ' + keplerianOrbit.aop);
+    expect(output).toContain(name + '.TA = ' + keplerianOrbit.ta);
+  });
+
+});
---
+++ server/lib/script-building-blocks/spacecraft/spacecraft-script-builder.ts
@@ -0,0 +1,105 @@
+export interface Epoch {
+  dateFormat: string;
+  epoch: string;
+}
+
+export interface CartesianOrbit {
+  system: 'cartesian';
+  x: number;
+  y: number;
+  z: number;
+  vx: number;
+  vy: number;
+  vz: number;
+}
+
+export interface KeplerianOrbit {
+  system: 'keplerian';
+  sma: number;
+  ecc: number;
+  inc: number;
+  raan: number;
+  aop: number;
+  ta: number;
+}
+
+
+export function spacecraftScriptBuilder(
+  name: string,
+  epoch?: Epoch,
+  orbit?: CartesianOrbit | KeplerianOrbit
+): string {
+
+  let lines = [];
+
+  // name
+  lines.push('Create Spacecraft ' + name + ';');
+
+  // epoch
+
+  if (epoch) {
+    lines.push(name + '.DateFormat = ' + epoch.dateFormat + ';');
+    lines.push(name + '.Epoch = ' + epoch.epoch + ';');
+  }
+
+  // initial orbit
+  if (orbit) {
+
+    if (orbit.system === 'cartesian') {
+
+      if (orbit.x) {
+        lines.push(name + '.X = ' + orbit.x + ';');
+      }
+
+      if (orbit.y) {
+        lines.push(name + '.Y = ' + orbit.y + ';');
+      }
+
+      if (orbit.z) {
+        lines.push(name + '.Z = ' + orbit.z + ';');
+      }
+
+      if (orbit.vx) {
+        lines.push(name + '.VX = ' + orbit.vx + ';');
+      }
+
+      if (orbit.vy) {
+        lines.push(name + '.VY = ' + orbit.vy + ';');
+      }
+
+      if (orbit.vz) {
+        lines.push(name + '.VZ = ' + orbit.vz + ';');
+      }
+
+    } else if (orbit.system === 'keplerian') {
+
+      if (orbit.sma) {
+        lines.push(name + '.SMA = ' + orbit.sma + ';');
+      }
+
+      if (orbit.ecc) {
+        lines.push(name + '.ECC = ' + orbit.ecc + ';');
+      }
+
+      if (orbit.inc) {
+        lines.push(name + '.INC = ' + orbit.inc + ';');
+      }
+
+      if (orbit.raan) {
+        lines.push(name + '.RAAN = ' + orbit.raan + ';');
+      }
+
+      if (orbit.aop) {
+        lines.push(name + '.AOP = ' + orbit.aop + ';');
+      }
+
+      if (orbit.ta) {
+        lines.push(name + '.TA = ' + orbit.ta + ';');
+      }
+    }
+
+  }
+
+  return lines.join('\n');
+};
+

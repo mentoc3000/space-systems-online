6e371635973af06fff45539aaec73797c92ae786
JP Sheehan <jp.sheehan2@gmail.com>
2017-02-02 17:56:03 -0500

    added placeholder spec

--- server/lib/gmat.spec.ts
+++ server/lib/gmat.spec.ts
@@ -1,55 +1,56 @@
 // let gmat = require('./gmat');
 import {
-   // runSim,
-   runScript
+  // runSim,
+  runScript
 } from './gmat';
 
 import fs = require('fs');
 import path = require('path');
 
 describe('GMAT module tests', () => {
-   let script: string;
-   // let output: string;
-   let expectedOutput: string;
-   // let filename: string;
+  let script: string;
+  // let output: string;
+  let expectedOutput: string;
+  // let filename: string;
 
-   describe('runScript', () => {
+  describe('runScript', () => {
 
-      let scripts = [
-         'Ex_HohmannTransfer',
-         'Ex_MarsBPlane',
-         'Ex_GEOTransfer'
-      ];
+    let scripts = [
+      'Ex_HohmannTransfer',
+      'Ex_MarsBPlane',
+      'Ex_GEOTransfer'
+    ];
 
-      scripts.forEach(testScript);
+    scripts.forEach(testScript);
 
-      function testScript(filename: string) {
-         it('solve the ' + filename + ' sample', function(){
-            script = readScript(filename);
-            expectedOutput = readOutput(filename);
-            runScript(script)
-               .then(function(output: string){
-                  expect(output).toEqual(expectedOutput);
-               });
-         });
-      }
-
-      function readScript(filename: string): string {
-         let dir = path.join(__dirname, './gmat-dist/R2016a/samples/');
-         let filepath = dir + filename + '.script';
-         return fs.readFileSync(filepath, 'utf8');
-      }
-   });
-
-   describe('runSim', () => {
-
-      it('runs basic propagation', function() {
+    function testScript(filename: string) {
+      it('solve the ' + filename + ' sample', function() {
+        script = readScript(filename);
+        expectedOutput = readOutput(filename);
+        runScript(script)
+          .then(function(output: string) {
+            expect(output).toEqual(expectedOutput);
+          });
       });
-   });
+    }
 
-   function readOutput(filename: string) {
-      let dir = path.join(__dirname, 'testScripts/');
-      let filepath = dir + filename + '.output';
+    function readScript(filename: string): string {
+      let dir = path.join(__dirname, './gmat-dist/R2016a/samples/');
+      let filepath = dir + filename + '.script';
       return fs.readFileSync(filepath, 'utf8');
-   }
+    }
+  });
+
+  xdescribe('runSim', () => {
+
+    it('runs basic propagation', function() {
+      expect('').toEqual('COMPLETE ME');
+    });
+  });
+
+  function readOutput(filename: string) {
+    let dir = path.join(__dirname, 'testScripts/');
+    let filepath = dir + filename + '.output';
+    return fs.readFileSync(filepath, 'utf8');
+  }
 });
